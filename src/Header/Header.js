81439deb5b76de2c39bf4740cf3647e5f775b515
Sara Mayberry <saraamayberry@yahoo.com>
2019-08-27 20:06:49 -0600

    init

---
+++ src/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+import {Link} from 'react-router-dom';
+
+
+export default function header(){
+    return(
+        <header class='header'>
+            <img className='logo' alt='logo' src='https://www.childhood.org.au/app/uploads/2017/07/ACF-logo-placeholder.png' />
+            <ul className='header-links'>
+                <Link to='our-story'>Our Story</Link>
+                <Link to='impact'>Impact</Link>
+                <Link to='contact'>Contact</Link>
+                <Link to='inspiration'>Inspiration</Link>
+            </ul>
+        </header>
+    )
+}
