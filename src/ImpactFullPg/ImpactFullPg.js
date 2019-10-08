4450c56224642edac1a8fd23ed219bffed8d4729
Sara Mayberry <saraamayberry@yahoo.com>
2019-10-08 15:34:58 -0600

    styling-new-info

--- src/ImpactFullPg/ImpactFullPg.js
+++ src/ImpactFullPg/ImpactFullPg.js
@@ -1,19 +1,64 @@
 import React from 'react';
 import './ImpactFullPg.css';
-// import ImpactImg from './background.jpg';
+import {Link} from 'react-router-dom';
+import animateScrollTo from 'animated-scroll-to';
 
 
-export default function ImpactFullPg(){
-    return(
-        <section className='impact-page'>
-            <div className='impact-full'>
-                {/* <div id='photo-background'>
-                    <img src={ImpactImg} className='impact-photo-main' alt='impact'/>
-                </div> */}
-                    <h2 className='impact-header-main'>Impact</h2>
-                    <p>There are several differences between SandPuzzle House and similar suppliers to the market, both in product and vision. SandPuzzle House addresses and delivers in almost ALL the requirements to turn a shelter into an actual home suited for long-term occupancy. Unlike other products that provide little less than the basics, SandPuzzle was designed to provide a safe, secure, solid structure using local materials and manpower. The structure can be erected in a couple of hours with little or no tools or hardware using unskilled labor. Not only does this reduce the cost of the final product, but it also satisfies our goal of building communities through job creation and instilling a sense of pride. Although other products currently available might have a slightly lower price point, these “savings” come at a much higher price as they tend to only provide in the very basic requirements as a short-term shelter.
-                    </p>
-            </div>
-        </section>    
-    )
+class ImpactFullPg extends React.Component{
+    solutionScroll() {
+        animateScrollTo(document.querySelector('.main-list'))
+    }
+
+    render(){
+        return(
+            <section className='impact-page'>
+                <div className='impact-full'>
+                        <h2 className='impact-header-main'>Impact</h2>
+                        <p>Several modular construction designs exist today and have been developed in order to try and
+                        address the ever-growing housing crises the world is facing. However, these designs do not incorporate
+                        some of the critical requirements that low-cost housing should provide to make it a viable options and to
+                        transform it from just a shelter to an actual home for long-term occupation. </p>
+    
+                        <p>Sand Puzzle House addresses the issues that most other designs overlooked. The structure was put through rigorous testing involving simulated hail / projectile impact of over 100 mph, water pressure of more than 200 psi, and winds of over 200 mph … and passed all tests. This makes it suitable for deployment in areas with severe weather and/or as emergency housing during severe weather events like hurricanes and storms. 
+                        </p>
+    
+                        <p>Sand Puzzle House addresses the issues that most other designs overlooked. The structure was put through rigorous testing involving simulated hail / projectile impact of over 100 mph, water pressure of more than 200 psi, and winds of over 200 mph … and passed all tests. This makes it suitable for deployment in areas with severe weather and/or as emergency housing during severe weather events like hurricanes and storms.</p>
+                        <p>As a result of the absence of localized stress points in the structure due to the elimination of hardware and fasteners, panels are also allowed to move in relation to one another without catastrophic failure that could lead to injury or death, making it an excellent option for deployment in seismic areas, both before any seismic events and as emergency housing directly after an event.
+                        As informal settlements and refugee camps are notorious for the high levels of crime and easy access to weapons, it was important that the structure could withstand penetration of some of the most common calibers of ammunition found in said locations. So, it was subjected to small arms fire including 9mm, 45 ACP and AK47 rounds and none of these ammunitions penetrated the structure, making it ideal as a safe option for deployment in high crime areas and countries with a history of war and violence.</p>
+                        <p>
+                        As most current structures in informal settlements are constructed out of highly flammable materials with little or no access for firefighting equipment. With no firefighting resources on location and only limited escape avenues, as well as most household still using open flame as a source of heat and light, fires are an all too common occurrence resulting in multiple deaths. Most of these deaths usually include small children and the elderly. So, it was important that the structure had to be fire-resistant, providing not just a means of combating the flames but also providing a larger window for possible escape from an inferno.  To aid in this, as well as to provide access to one of the most basic requirements for survival, namely water, the design also incorporates a water storage facility. Not only is this part of the fire safety aspect, it also ensures safe storage to this valuable commodity that can be used for household purposes, as well as to improve hygienic conditions in the area. In turn, this will help in the fight against the spread of infectious diseases in these areas.  
+                        </p>
+                </div>
+                <div className='impact-list'>
+                    <Link to='/impact' className='solution-link'>
+                        <button onClick={this.solutionScroll} className='solution-button'>Our Solution</button>
+                    </Link>
+                    <h3 className='solution-h3'>Our Solution</h3>
+                        <ul className='main-list'> 
+                            <li>
+                            Sand Puzzle House addresses the following issues most often overlooked by other designs:
+                            </li>
+                                <ul>
+                                <li>Low Cost</li> 
+                                <li>Ease of deployment</li>
+                                <li>Ease of expanding existing strctures</li>
+                                <li>Withstanding extreme weather elements</li>
+                                <li>Safe and secure</li>
+                                <li>Built resistant</li>
+                                <li>Fire resistant</li>
+                                <li>No skilled labor required to erect</li>
+                                <li>No special tools required</li>
+                                <li>Minimal hardware</li>
+                                <li>Incorporated water storage with ease of connecting to central water source</li>
+                                <li>Mobile structure with the characteristics of a permanent structure</li>
+                                </ul>
+                        </ul>
+    
+                </div>
+            </section>    
+    
+        )
+    }
 } 
+
+export default ImpactFullPg;
