1a3ba17179a70dfab21f998a4c13e92160c7c64f
Sara Mayberry <saraamayberry@yahoo.com>
2019-10-13 10:41:32 -0600

    impact-whitespace-fix

--- src/ImpactFullPg/ImpactFullPg.js
+++ src/ImpactFullPg/ImpactFullPg.js
@@ -6,11 +6,11 @@ import animateScrollTo from 'animated-scroll-to';
 
 class ImpactFullPg extends React.Component{
     solutionScroll() {
-        animateScrollTo(document.querySelector('.main-list'))
+        animateScrollTo(document.querySelector('.main-list-scroll'))
     }
 
     render(){
-        return(
+        return( 
             <section className='impact-page'>
                 <div className='impact-full'>
                         <h2 className='impact-header-main'>Impact</h2>
@@ -22,8 +22,7 @@ class ImpactFullPg extends React.Component{
                         <p>Sand Puzzle House addresses the issues that most other designs overlooked. The structure was put through rigorous testing involving simulated hail / projectile impact of over 100 mph, water pressure of more than 200 psi, and winds of over 200 mph … and passed all tests. This makes it suitable for deployment in areas with severe weather and/or as emergency housing during severe weather events like hurricanes and storms. 
                         </p>
     
-                        <p>Sand Puzzle House addresses the issues that most other designs overlooked. The structure was put through rigorous testing involving simulated hail / projectile impact of over 100 mph, water pressure of more than 200 psi, and winds of over 200 mph … and passed all tests. This makes it suitable for deployment in areas with severe weather and/or as emergency housing during severe weather events like hurricanes and storms.</p>
-                        <p>As a result of the absence of localized stress points in the structure due to the elimination of hardware and fasteners, panels are also allowed to move in relation to one another without catastrophic failure that could lead to injury or death, making it an excellent option for deployment in seismic areas, both before any seismic events and as emergency housing directly after an event.
+                        <p className='main-list-scroll'>As a result of the absence of localized stress points in the structure due to the elimination of hardware and fasteners, panels are also allowed to move in relation to one another without catastrophic failure that could lead to injury or death, making it an excellent option for deployment in seismic areas, both before any seismic events and as emergency housing directly after an event.
                         As informal settlements and refugee camps are notorious for the high levels of crime and easy access to weapons, it was important that the structure could withstand penetration of some of the most common calibers of ammunition found in said locations. So, it was subjected to small arms fire including 9mm, 45 ACP and AK47 rounds and none of these ammunitions penetrated the structure, making it ideal as a safe option for deployment in high crime areas and countries with a history of war and violence.</p>
                         <p>
                         As most current structures in informal settlements are constructed out of highly flammable materials with little or no access for firefighting equipment. With no firefighting resources on location and only limited escape avenues, as well as most household still using open flame as a source of heat and light, fires are an all too common occurrence resulting in multiple deaths. Most of these deaths usually include small children and the elderly. So, it was important that the structure had to be fire-resistant, providing not just a means of combating the flames but also providing a larger window for possible escape from an inferno.  To aid in this, as well as to provide access to one of the most basic requirements for survival, namely water, the design also incorporates a water storage facility. Not only is this part of the fire safety aspect, it also ensures safe storage to this valuable commodity that can be used for household purposes, as well as to improve hygienic conditions in the area. In turn, this will help in the fight against the spread of infectious diseases in these areas.  
